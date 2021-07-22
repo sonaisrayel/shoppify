d49af6b9ec297be60f886b876bdbf4f556ad5776
Harut Melkonyan <harut15harut@gmail.com>
2021-07-22 13:50:02 +0400

    task done (#9)
    Well done

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
 const PORT = '3000';
-
-
-let  users = require('./user.json');
-
+let  users = require('./storage/users.json');
+let products = require("./storage/product.json");
 
 app.get('/users', (req, res) => {
     res.status(200).send(users)
 })
-
+app.get("/products",(req,res)=>{
+res.status(200).send(products);
+});
 
 app.listen(PORT, console.log(`Server listen to port ${PORT}`))
