7bbc5a46cb35ec0f41cba97e3ed6460ea710c3bb
Harut Melkonyan <harut15harut@gmail.com>
2021-07-22 19:37:13 +0400

    OOP class

--- errors/message.js
+++ errors/message.js
@@ -1,6 +1,13 @@
-function messages(Cmessage,Cstatus){
-this.returnList = {message:Cmessage, status:Cstatus};
+class Messages {
+    constructor(message,status){
+        this.message = message;
+        this.status = status;
+        this.returnList = {message:this.message, status:this.status};
+    }
+returnMsg(){
+    return this.returnList;
+}
 }
 
-let mess = new messages("this is message",200);
-module.exports = mess.returnList;
+let mess = new Messages("this is messages",200);
+module.exports = mess.returnMsg();
