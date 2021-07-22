4128fd8508ba6495864d0b7105cc3bacc978d65d
Sona Israyelyan <sonai@brightcom.com>
2021-07-22 23:02:20 +0400

    Add index.js changes

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
 const PORT = '3000';
-let  users = require('./storage/users.json');
+let users = require('./storage/users.json');
 let products = require("./storage/product.json");
 
 app.get('/users', (req, res) => {
     res.status(200).send(users)
 })
-app.get("/products",(req,res)=>{
-res.status(200).send(products);
+
+app.get("/products", (req, res) => {
+    res.status(200).send(products);
 });
 
 app.listen(PORT, console.log(`Server listen to port ${PORT}`))
