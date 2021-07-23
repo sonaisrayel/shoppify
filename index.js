ca791343213cb4cf27270b96a2e706a23f21f908
Tatev9 <grigoryantateev@gmail.com>
2021-07-23 20:22:56 +0400

    Add category

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 const PORT = '3000';
 let users = require('./storage/users.json');
 let products = require("./storage/product.json");
+let category = require("./storage/category.json")
+
 
  app.get("/message",(req,res)=>{
  res.status(messages.status).send(messages.message);
@@ -12,8 +14,11 @@ let products = require("./storage/product.json");
 app.get('/users', (req, res) => {
     res.status(200).send(users)
 })
+app.get('/categories', (req, res)=>{
+    res.status(200).send(category)
+})
 
-app.get("/products", (req, res) => {
+app.get("/products", (req, res) => { //localhost:300/users
     res.status(200).send(products);
 });
 
