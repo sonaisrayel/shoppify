08be7d72e0ccbfc913fd16d4232c7a288125f107
sonaisrayel <israyelyan.sona@gmail.com>
2021-07-24 21:52:12 +0400

    Add 24.07.2021 Classwork to git

--- index.js
+++ index.js
@@ -1,43 +1,30 @@
 const express = require('express');
-const messages = require("./errors/message");
 const app = express();
-const PORT = '3000';
- let users = require('./storage/users.json');
+const config = require('config');
+const products = require("./storage/product.json");
+const orders = require('./storage/orders.json');
+const category = require("./storage/category.json");
+const users = require('./storage/users.json').users
+const PORT =  config.get('PORT');
 
-let products = require("./storage/product.json");
-
-
-
-let orders = require('./storage/orders.json');
-
-let category = require("./storage/category.json")
-
-
-
-
-
- app.get("/message",(req,res)=>{
- res.status(messages.status).send(messages.message);
-});
-
-app.get('/users', (req, res) => {
-    res.status(200).send(users)
-})
-app.get('/categories', (req, res)=>{
+app.get('/categories', (req, res) => {
     res.status(200).send(category)
 })
 
-
-app.get('/orders', (req,res)=> {
-    res.send(oreders)
+app.get('/orders', (req, res) => {
+    res.send(orders)
 })
 
+//localhost:3000/products
 app.get("/products", (req, res) => {
-
-app.get("/products", (req, res) => { //localhost:300/users
-
     res.status(200).send(products);
 });
 
-app.listen(PORT, console.log(`Server listen to port ${PORT}`))
+//get one user localhost:3000/users/1
+app.get("/users/:userId", (req, res) => {
+    const { userId } = req.params;
+    const user = users.find(user => user.id === userId);
+    res.send(user)
+})
 
+app.listen(PORT, console.log(`Server listen to port ${PORT}`))
