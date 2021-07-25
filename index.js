d8fedbab301f66ffdfa4caae34d4b12ab89b4fed
Harut Melkonyan <harut15harut@gmail.com>
2021-07-25 15:00:42 +0400

    API-4

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
 const express = require('express');
 const app = express();
-const PORT = '3000';
-let users = require('./storage/users.json');
 let products = require("./storage/product.json");
 let orders = require("./storage/orders.json");
+const config = require('./config/default.json');
+const categories = require("./storage/category.json");
+const users = require('./storage/users.json');
+const PORT =  config.PORT;
 app.get("./message", (req, res) => {
     res.status(messages.status).send(messages.message);
 });
 
-app.get('./users', (req, res) => {
+app.get('/users', (req, res) => {
     res.status(200).send(users)
-})
+});
 app.get("./orders/1", (req, res) => {
     res.status(200).send(orders[0]);
 });
-
-const config = require('config');
-const products = require("./storage/product.json");
-const orders = require('./storage/orders.json');
-const category = require("./storage/category.json");
-const users = require('./storage/users.json').users
-const PORT =  config.get('PORT');
-
 app.get('/categories', (req, res) => {
-    res.status(200).send(category)
+    res.status(200).send(categories)
 })
-
+app.get('/categories/:catid', (req, res) => {
+    let {catid} = req.params;
+    let category = categories.categories.find(category=>category.id==catid);
+    res.status(200).send(category)
+});
 app.get('/orders', (req, res) => {
     res.send(orders)
-})
+});
 
 //localhost:3000/products
 
@@ -39,7 +37,7 @@ app.get("/products", (req, res) => {
 //get one user localhost:3000/users/1
 app.get("/users/:userId", (req, res) => {
     const { userId } = req.params;
-    const user = users.find(user => user.id === userId);
+    const user = users.users.find(user => user.id === userId);
     res.send(user)
 })
 
