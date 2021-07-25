46527d3a89885f02d717b1c39b17c315e776f8d7
unknown <bdoyan.mxitar@gmail.com>
2021-07-25 23:49:50 +0400

    API_10

--- index.js
+++ index.js
@@ -9,40 +9,40 @@ let orders = require("./storage/orders.json");
 
 app.get("/message", (req, res) => {
     res.status(messages.status).send(messages.message);
-
 });
 
 app.get('/user', (req, res) => {
     res.status(200).send(users.users[0])   
-
-    // res.send("yes")
 })
 app.get('/users', (req, res) => {
     res.status(200).send(users)
-    
 })
-
 app.get("/products", (req, res) => {
     res.status(200).send(products);
-
 });
 app.get("/product", (req, res) => {
     res.status(200).send(products.products[0]);
 });
-
 app.get("/categorys", (req, res) => {
     res.status(200).send(category);
 });
 app.get("/category", (req, res) => {
     res.status(200).send(category.categories[0]);
 });
-  
+
 app.get("/orders", (req, res) => {
     res.status(200).send(orders);
 });
 app.get("/order", (req, res) => {
     res.status(200).send(orders.categories[0]);
-  });
+});
+app.delete("/orders/:id", (req, res) => {
+    const { id } = req.params;
+    let order = orders.find(order => order.id == id);
+    orders.splice(id - 1, 1);
+    res.status(200).send(order);
+    
+});
 
 
 app.listen(PORT, console.log(`Server listen to port ${PORT}`))
