6e0d12865b99c24dfb3805445566cfb39c9c6a83
Harut Melkonyan <harut15harut@gmail.com>
2021-07-26 00:19:05 +0400

    Api 16 (#38)
    * API-11
    
    * API-11
    
    * API-16
    
    Co-authored-by: Sona Israyelyan <israyelyan.sona@gmail.com>

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const orders = require("./storage/orders.json");
 const categories = require("./storage/category.json");
 const users = require('./storage/users.json');
 
+
 const PORT = config.get('PORT');
 const app = express();
 
@@ -22,18 +23,17 @@ app.get("/users/:userId", (req, res) => {
     const { userId } = req.params;
     const user = users.find(user => user.id === userId);
     res.send(user)
-})
-
+});
 
 //PRODUCTS ROUTES
 app.get("/products", (req, res) => {
     res.status(200).send(products);
 });
 
-app.delete("/products/:id", (req, res) => {
+app.delete("/products/:productId", (req, res) => {
     const { id } = req.params;
-    let product = products.find(product => product.id == id);
-    products.splice(id-1, 1);
+    let product = products.find(product => product.id == productId);
+    products.splice(id-1,1);
     res.status(200).send(product);
 });
 
@@ -47,14 +47,20 @@ app.get("/orders/1", (req, res) => {
     res.status(200).send(orders[0]);
 });
 
+app.patch("/orders/:id", (req, res) => {
+    const { id } = req.params;
+    orders.find(order => order.id == id).name = "new name";
+    res.status(200).send(orders);
+});
+
 //CATEGORY ROUTES
 app.get('/categories', (req, res) => {
     res.status(200).send(categories)
 })
 
-app.get('/categories/:catid', (req, res) => {
+app.get('/categories/:catId', (req, res) => {
     const { catid } = req.params;
-    const category = categories.find(category => category.id == catid);
+    const category = categories.find(category => category.id == catId);
     res.status(200).send(category)
 });
 
