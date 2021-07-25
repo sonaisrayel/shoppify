c8fb5109f0ffb55ca826105aa04735bbd2ee1e2e
Sona Israyelyan <israyelyan.sona@gmail.com>
2021-07-25 18:01:26 +0400

    Api 11 (#33)
    * Add index.js changes
    
    * Add api design
    
    * Add api design
    
    * Add changes to api design
    
    Co-authored-by: Sona Israyelyan <sonai@brightcom.com>

--- index.js
+++ index.js
@@ -1,37 +1,20 @@
 const express = require('express');
-const app = express();
-let products = require("./storage/product.json");
-let orders = require("./storage/orders.json");
-const config = require('./config/default.json');
-const categories = require("./storage/category.json");
-const users = require('./storage/users.json');
-const PORT =  config.PORT;
-app.get("./message", (req, res) => {
-    res.status(messages.status).send(messages.message);
-});
+const config = require('config');
 
+//getting json files
+const products = require("./storage/product.json");
+const orders = require("./storage/orders.json");
+const categories = require("./storage/category.json");
+const users = require('./storage/users.json').users;
+
+const PORT = config.get('PORT');
+const app = express();
+
+
+//USERS ROUTES
+//GET USERS
 app.get('/users', (req, res) => {
     res.status(200).send(users)
-});
-app.get("./orders/1", (req, res) => {
-    res.status(200).send(orders[0]);
-});
-app.get('/categories', (req, res) => {
-    res.status(200).send(categories)
-})
-app.get('/categories/:catid', (req, res) => {
-    let {catid} = req.params;
-    let category = categories.categories.find(category=>category.id==catid);
-    res.status(200).send(category)
-});
-app.get('/orders', (req, res) => {
-    res.send(orders)
-});
-
-//localhost:3000/products
-
-app.get("/products", (req, res) => {
-    res.status(200).send(products);
 });
 
 //get one user localhost:3000/users/1
@@ -40,5 +23,37 @@ app.get("/users/:userId", (req, res) => {
     const user = users.users.find(user => user.id === userId);
     res.send(user)
 })
+
+
+//PRODUCTS ROUTES
+app.get("/products", (req, res) => {
+    res.status(200).send(products);
+});
+
+//ORDERS ROUTES
+
+app.get('/orders', (req, res) => {
+    res.send(orders)
+});
+
+app.get("/orders/1", (req, res) => {
+    res.status(200).send(orders[0]);
+});
+
+//CATEGORY ROUTES
+app.get('/categories', (req, res) => {
+    res.status(200).send(categories)
+})
+
+app.get('/categories/:catid', (req, res) => {
+    const { catid } = req.params;
+    const category = categories.categories.find(category => category.id == catid);
+    res.status(200).send(category)
+});
+
+
+app.get("/message", (req, res) => {
+    res.status(messages.status).send(messages.message);
+});
 
 app.listen(PORT, console.log(`Server listen to port ${PORT}`))
