a4a4b8e8747d68e82568cae4dd66a3fcc99f71bb
sonaisrayel <israyelyan.sona@gmail.com>
2021-07-26 20:22:56 +0400

    UPDATE PRODUCT BY ID

--- index.js
+++ index.js
@@ -5,31 +5,19 @@ const config = require('config');
 const products = require("./storage/product.json");
 const orders = require("./storage/orders.json");
 const categories = require("./storage/category.json");
-const users = require('./storage/users.json');
+const users = require("./storage/users.json");
 
 
-const PORT = config.get('PORT');
+const PORT = config.get("PORT");
 const app = express();
- main
-
 
 //USERS ROUTES
 //GET USERS
 
-const PORT = '3000';
-let users = require('./storage/users.json');
-let products = require("./storage/product.json");
-let category = require("./storage/category.json");
-let orders = require("./storage/orders.json");
-
-app.get("/message", (req, res) => {
-    res.status(messages.status).send(messages.message);
-});
-
 app.get('/user', (req, res) => {
     res.status(200).send(users.users[0])   
 })
- API_ALL
+ 
 app.get('/users', (req, res) => {
     res.status(200).send(users)
 });
@@ -56,7 +44,6 @@ app.delete("/products/:productId", (req, res) => {
 });
 
 //ORDERS ROUTES
-
 app.get('/orders', (req, res) => {
     res.send(orders)
 });
@@ -74,13 +61,14 @@ app.patch("/orders/:id", (req, res) => {
 //CATEGORY ROUTES
 app.get('/categories', (req, res) => {
     res.status(200).send(categories)
-})
- main
+});
+ 
 
 app.get('/categories/:catId', (req, res) => {
     const { catid } = req.params;
     const category = categories.find(category => category.id == catId);
     res.status(200).send(category)
+})
 
 app.get("/products", (req, res) => {
     res.status(200).send(products);
@@ -90,7 +78,7 @@ app.get("/product", (req, res) => {
 });
 app.get("/categorys", (req, res) => {
     res.status(200).send(category);
- API_ALL
+ 
 });
 app.get("/category", (req, res) => {
     res.status(200).send(category.categories[0]);
@@ -102,6 +90,7 @@ app.get("/orders", (req, res) => {
 app.get("/order", (req, res) => {
     res.status(200).send(orders.categories[0]);
 });
+
 app.delete("/orders/:id", (req, res) => {
     const { id } = req.params;
     let order = orders.find(order => order.id == id);
@@ -110,7 +99,4 @@ app.delete("/orders/:id", (req, res) => {
     
 });
 
-
-
 app.listen(PORT, console.log(`Server listen to port ${PORT}`))
-
