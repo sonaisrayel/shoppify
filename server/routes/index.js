5fd0005055782e6897b8c4bf4a7a75b11b8670be
unknown <bdoyan.mxitar@gmail.com>
2021-07-26 23:41:55 +0400

    DELETE ORDER BY ID

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,2 +1,4 @@
 module.exports.UserRouter =  require('./user-route');
 module.exports.ProductRouter = require('./product-route');
+module.exports.OrderRouter = require('./orders-route');
+module.exports.CategoryRouter = require('./categories-ruute');
