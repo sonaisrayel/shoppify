0ec648f152e089e147ec4615d80948fcd6d81bed
Tatev <tatevs85@gmail.com>
2021-07-30 00:07:39 +0400

    add all category

--- server/routes/categories-route.js
+++ server/routes/categories-route.js
@@ -1,29 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const categories = require("../models/category.json");
+const { getCategorys, getCategory, deleteCategory, updateCategory } = require('../controllers/category-controller')
 
-router.get('/', (req, res) => {
-    res.status(200).send(categories)
-});
+router.get('/',getCategorys);
 
-router.get('/:catId', (req, res) => {
-    const { catId } = req.params;
-    const category = categories.find(category => category.id == catId);
-    res.status(200).send(category)
-}) 
+router.get('/:catId', getCategory) 
 
-router.delete("/:catId", (req, res) => {
-    const { catId } = req.params;
-    //const category = categories.find(category => category.id == catId);
-     categories.splice(catId - 1, 1);
-    res.status(200).send(categories);
-});
+router.delete("/:catId", deleteCategory);
 
-router.patch('/:catId', (req,res)=> {
-    const{ catId } = req.params;
-    categories.find(category => JSON.stringify(category.id) === catId).name = "new name";
-    res.status(200).send(categories);
-})
+router.patch('/:catId', updateCategory )
 
 module.exports = router;
