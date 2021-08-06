4570cf8c984893c6a693e13ed1567f45f861ec64
Tatev9 <grigoryantateev@gmail.com>
2021-08-06 18:25:05 +0400

    Create User

---
+++ server/models/user-model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: String,
+        required: true
+    },
+    salary: {
+        type: String,
+        required: true
+    }
+   
+});
+
+const UserModel = mongoose.model('User', UserSchema);
+
+module.exports = UserModel
