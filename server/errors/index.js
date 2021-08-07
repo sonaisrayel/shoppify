be645dad573209da244e71cf894c91b2457ba5f4
harut17harut <harut15harut@gmail.com>
2021-08-07 20:20:28 +0400

    API-54

--- server/errors/index.js
+++ server/errors/index.js
@@ -1,8 +1,9 @@
 const NotFoundError = require("./not-found-error");
-
+const NotModifiedError = require("./not-modified-error");
 
 
 
 module.exports = {
-    NotFoundError
+    NotFoundError,
+    NotModifiedError
 }
