5220211b967bf3cd28abac7162fc81118585e542
Tatev9 <grigoryantateev@gmail.com>
2021-08-07 16:40:00 +0400

    Get One User

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -3,7 +3,17 @@ const NotFoundError = require('../errors/not-found-error')
 
 
 async function getUser(req,res){
-
+    try {
+        const { userId } = req.params;
+        const user = await UserModel.findOne({ _id: favoritId })
+        if (user) {
+            res.status(200).send(user)
+        } else {
+            res.status(404).send({ error: "The user not found" })
+        }
+    } catch (error) {
+        res.send(error)
+    }
 }
 
 
