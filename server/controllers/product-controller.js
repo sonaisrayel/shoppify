cdbb1b1d71571183afbd9072f31b0848841f9cdf
VLAD <vladimirbaghdasrayan7@gmail.com>
2021-08-08 22:59:56 +0400

    API-37

---
+++ server/controllers/product-controller.js
@@ -0,0 +1,27 @@
+const { NotFoundError } = require('../errors/index');
+const { productModel } = require("../models");
+//GET ONE PRODUCT
+async function getproduct(req, res) {
+    const { productId } = req.params;
+    const product = await productModel.find({ _id: productId })
+    try {
+        if (!product) {
+            throw new NotFoundError("product not found")
+        }
+        res.status(200).send(product)
+    }
+    catch (error) {
+        res.status(404).send(error.message)
+    }
+}
+async function getproduct(req, res) {
+    try {
+        const product = await productModel.find()
+        if (!orders) {
+            throw new NotFoundError('product not found')
+        }
+        res.send(product)
+    } catch (error) {
+        res.send(error.message)
+    }
+}
